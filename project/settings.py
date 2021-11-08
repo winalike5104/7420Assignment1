90893eeb985dfbfbbb0a828053bc1b10a94d9fb4
windalike5104 <windalike5104@gmail.com>
2021-11-08 22:22:08 +0800

    change

--- project/settings.py
+++ project/settings.py
@@ -134,13 +134,12 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    'mystatic',
+]
 django_heroku.settings(locals())
 
 # Default primary key field type
