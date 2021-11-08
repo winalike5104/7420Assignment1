704f75d8bf3ba58f1ae8080fee87b455cbd7b2ae
windalike5104 <windalike5104@gmail.com>
2021-11-08 21:20:55 +0800

    modify setting

--- project/settings.py
+++ project/settings.py
@@ -134,10 +134,12 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    (os.path.join(BASE_DIR,'static'))
-]
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = "/static/"
 django_heroku.settings(locals())
 
